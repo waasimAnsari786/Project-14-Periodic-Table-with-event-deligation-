4fc6cf2c0981da1d75c147bd7b791e0e4223890e
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-20 05:17:51 -0700

    Update style.js
    I gave a better UI to my mini project.

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ let body = document.querySelector("body");
 const initializeTable = () => {
     let mainDiv = document.createElement("div");
     mainDiv.classList.add("main-div" , "container");
+
+    let elementval = document.createElement("p");
+    elementval.classList.add("element-val");
     let index; 
 
     for (index = 1; index <= 50 ; index++) {
         let elements = document.createElement("p");
         elements.classList.add("elements");
+
 
         if (index < 10) {
             elements.innerText = `element 0${index}`;
@@ -17,17 +21,22 @@ const initializeTable = () => {
         }
 
         mainDiv.append(elements);
+        body.append(elementval);
         body.append(mainDiv);
     }
 
-    return [mainDiv , index]
+    return [mainDiv , index , elementval]
 };
 
-let [container , index] = initializeTable();
+let [container , index , pera] = initializeTable();
+console.log(pera);
 
 const printDataOfTable = (evt) => {
-    let splitedVal = evt.target.innerText.split(" ");
-    alert(`Atomic Number: ${splitedVal[1]} \nElemnt's Name: ${evt.target.innerText}`);
+    if (evt.target.classList.contains("elements")) {
+        evt.target.style.border = `.1rem solid white`;
+        evt.target.style.boxShadow = `0 0 1rem .4rem #F2A1A3`;
+        let splitedVal = evt.target.innerText.split(" ");
+        pera.innerText = `Atomic Number: ${splitedVal[1]}, Elemnt's Name: ${evt.target.innerText}`;    }
 };
 
 container.addEventListener("click" , printDataOfTable);
