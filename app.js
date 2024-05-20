f71e50f7148d3b10d26d6dc25aba3230e10972bd
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-20 14:16:37 -0700

    Update style.css and app.js
    I deleted some lines of code from style.css and app.js

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ let body = document.querySelector("body");
 const initializeTable = () => {
     let mainDiv = document.createElement("div");
     mainDiv.classList.add("main-div" , "container");
-
-    let elementval = document.createElement("p");
-    elementval.classList.add("element-val");
     let index; 
 
     for (index = 1; index <= 50 ; index++) {
@@ -21,22 +18,19 @@ const initializeTable = () => {
         }
 
         mainDiv.append(elements);
-        body.append(elementval);
         body.append(mainDiv);
     }
 
-    return [mainDiv , index , elementval]
+    return [mainDiv , index]
 };
 
 let [container , index , pera] = initializeTable();
-console.log(pera);
 
 const printDataOfTable = (evt) => {
     if (evt.target.classList.contains("elements")) {
-        evt.target.style.border = `.1rem solid white`;
-        evt.target.style.boxShadow = `0 0 1rem .4rem #F2A1A3`;
         let splitedVal = evt.target.innerText.split(" ");
-        pera.innerText = `Atomic Number: ${splitedVal[1]}, Elemnt's Name: ${evt.target.innerText}`;    }
+        alert(`Atomic Number: ${splitedVal[1]}, Elemnt's Name: ${evt.target.innerText}`);
+    }
 };
 
 container.addEventListener("click" , printDataOfTable);
