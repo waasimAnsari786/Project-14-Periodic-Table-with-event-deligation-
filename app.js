af493f272646580ff670fe5e146921c3843dfc29
waasimAnsari786 <waasimansari92@gmail.com>
2024-05-20 03:09:30 -0700

    Update app.js
    I initialize this project with the help of JavaScript not html or css.

---
+++ app.js
@@ -0,0 +1,33 @@
+let body = document.querySelector("body");
+
+const initializeTable = () => {
+    let mainDiv = document.createElement("div");
+    mainDiv.classList.add("main-div" , "container");
+    let index; 
+
+    for (index = 1; index <= 50 ; index++) {
+        let elements = document.createElement("p");
+        elements.classList.add("elements");
+
+        if (index < 10) {
+            elements.innerText = `element 0${index}`;
+        }
+        else{
+            elements.innerText = `element ${index}`;
+        }
+
+        mainDiv.append(elements);
+        body.append(mainDiv);
+    }
+
+    return [mainDiv , index]
+};
+
+let [container , index] = initializeTable();
+
+const printDataOfTable = (evt) => {
+    let splitedVal = evt.target.innerText.split(" ");
+    alert(`Atomic Number: ${splitedVal[1]} \nElemnt's Name: ${evt.target.innerText}`);
+};
+
+container.addEventListener("click" , printDataOfTable);
